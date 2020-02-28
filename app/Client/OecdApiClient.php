50c3baa1739b850dfc4a36870e27d90695a67307
Pavel <samsonov.teamwork@gmail.com>
2020-02-28 17:38:09 +0200

    Oecd api

---
+++ app/Client/OecdApiClient.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Client;
+
+use Http\Client\HttpClient;
+use Http\Message\RequestFactory;
+use Psr\Http\Client\ClientExceptionInterface;
+
+class OecdApiClient
+{
+    /** @var HttpClient */
+    private $httpClient;
+
+    /** @var RequestFactory  */
+    private $requestFactory;
+
+    public function __construct(HttpClient $httpClient, RequestFactory $requestFactory)
+    {
+        $this->httpClient = $httpClient;
+        $this->requestFactory = $requestFactory;
+    }
+
+    public function sendRequest(string $path, string $method = 'get')
+    {
+        try {
+
+            $request = $this->requestFactory->createRequest($method, $path);
+            $response = $this->httpClient->sendRequest($request);
+
+            return json_decode($response->getBody()->getContents(), true);
+        } catch (ClientExceptionInterface $e) {
+            //todo: logging
+
+            throw $e;
+        }
+    }
+
+    public function getBliData()
+    {
+        $response = $this->sendRequest('BLI');
+        $res = [];
+        foreach ($response['dataSets'][0]['observations'] as $key => $values) {
+            $item = [];
+            foreach (explode(':', $key) as $index => $dimension) {
+                $name = $response['structure']['dimensions'][$index];
+            }
+            $res[] = $item;
+            return $values;
+        }
+        return $res;
+    }
+}
