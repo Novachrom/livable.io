78fe807d2825edeb959f9df893db6e2daa26df93
Pavel <samsonov.teamwork@gmail.com>
2020-03-03 17:38:41 +0200

    OpenLayers Map integration

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -10,6 +10,7 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
         <link rel="stylesheet" href="{{ url('css/style.css?v=').date('Y-m-d_H-i-s') }}">
+        @yield('head')
     </head>
     <body>
     <div class="content">
--- resources/views/cities.blade.php
+++ resources/views/cities.blade.php
@@ -1,8 +1,20 @@
 
 @extends('base')
 
+@section('head')
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.2.1/css/ol.css" type="text/css">
+    <style>
+        .map {
+            height: 400px;
+            width: 100%;
+        }
+    </style>
+    <script src="https://cdn.jsdelivr.net/gh/openlayers/openlayers.github.io@master/en/v6.2.1/build/ol.js"></script>
+@endsection
+
 @section('content')
     @parent
+
     <form class="form-inline" id="cost-filter-form" action="{{ request()->fullUrl() }}">
         <label class="sr-only" for="inlineFormInputName2">Search</label>
         <input type="number" name="cost_of_living_from" @if(request()->has('cost_of_living_from')) value="{{ request()->get('cost_of_living_from') }}" @endif class="form-control mb-2 mr-sm-2" id="search" placeholder="Cost of living from">
@@ -43,4 +55,125 @@
         </tbody>
     </table>
     <div class="content">{{ $cities->links() }}</div>
+    <div id="map" class="map"></div>
+    <div id="popup" class="ol-popup" style="display:none">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title" id="popup-title">City</h5>
+                <p class="card-text" id="popup-content">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-danger" id="popup-closer">Close</a>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript">
+
+        var cities = {!!  json_encode($cities) !!};
+        // console.log(cities);
+        function createMap(locations) {
+
+            var mapCenter = ol.proj.fromLonLat([ -74.0446426, 40.6892534 ]);
+            var view = new ol.View({
+                center: mapCenter,
+                zoom: 10
+            });
+
+            var map = new ol.Map({
+                target: 'map',
+                layers: [
+                    new ol.layer.Tile({
+                        source: new ol.source.OSM()
+                    })
+                ],
+                view: view
+            });
+
+            // Array of Icon features
+            var iconFeatures = [];
+            for (var i = 0; i < locations.length; i++) {
+
+                if(!locations[i].longitude && !locations[i].latitude) {
+                    continue;
+                }
+
+                var iconFeature = new ol.Feature({
+                    type: 'click',
+                    name: locations[i].name + ', ' + locations[i].country.name,
+                    geometry: new ol.geom.Point(ol.proj.transform([locations[i].longitude, locations[i].latitude], 'EPSG:4326', 'EPSG:3857')),
+                });
+
+                iconFeatures.push(iconFeature);
+            }
+
+            var vectorSource = new ol.source.Vector({
+                features: iconFeatures
+            });
+
+            var iconStyle = new ol.style.Style({
+                image: new ol.style.Icon(({
+                    anchor: [0.5, 1],
+                    src: "http://cdn.mapmarker.io/api/v1/pin?size=50&hoffset=1"
+                }))
+            });
+
+            var vectorLayer = new ol.layer.Vector({
+                source: vectorSource,
+                style: iconStyle,
+                updateWhileAnimating: true,
+                updateWhileInteracting: true,
+            });
+            map.addLayer(vectorLayer);
+
+            var container = document.getElementById('popup');
+            var content = document.getElementById('popup-content');
+            var closer = document.getElementById('popup-closer');
+
+            var overlay = new ol.Overlay({
+                element: container,
+                autoPan: true,
+                autoPanAnimation: {
+                    duration: 250
+                }
+            });
+            map.addOverlay(overlay);
+
+            closer.onclick = function() {
+                overlay.setPosition(undefined);
+                closer.blur();
+                return false;
+            };
+
+            map.on('singleclick', function(evt) {
+
+                // Hide existing popup and reset it's offset
+                // popup.hide();
+                // popup.setOffset([0, 0]);
+
+                // Attempt to find a feature in one of the visible vector layers
+                var feature = map.forEachFeatureAtPixel(evt.pixel, function(feature, layer) {
+                    return feature;
+                });
+
+                if (feature) {
+                    var coord = feature.getGeometry().getCoordinates();
+                    var props = feature.getProperties();
+                    content.innerHTML =
+                        // '<a style="color:black; font-weight:600; font-size:11px" href="http://www.somedomain.com/' + props.url + '">' +
+                        // '<img width="200" src="' +  props.image + '"  />' +
+                        '<div style="width:220px; margin-top:3px">' + props.name + '</div></a>';
+
+                    // Offset the popup so it points at the middle of the marker not the tip
+                    overlay.setPosition(evt.coordinate);
+                    container.style.display = 'block';
+                } else {
+                    overlay.setPosition(undefined);
+                    closer.blur();
+                    container.style.display = 'none';
+                }
+            });
+
+            return map;
+        }
+
+        var map = createMap(cities.data);
+    </script>
 @endsection
